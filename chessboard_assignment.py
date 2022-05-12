5a2d66854e237adcf872d716beb10ef3c961d54f
mmcaulif <60351960+mmcaulif@users.noreply.github.com>
2022-05-12 19:41:15 +0100

    cleaned up repo

--- chessboard_assignment.py
+++ chessboard_assignment.py
@@ -5,10 +5,10 @@ vid = cv2.VideoCapture(0)
 i = 0
 chessboard_flags = (cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
 corner_flags = (cv2.TERM_CRITERIA_EPS + cv2.TermCriteria_COUNT, 40, 0.001)
-board_dims = (7,7)
+board_dims = (10,7)
 
-objp = np.zeros((7*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+objp = np.zeros((np.prod(board_dims),3), np.float32)
+objp[:,:2] = np.mgrid[0:10,0:7].T.reshape(-1,2)
 
 objpoints = [] # 3d point in real world space
 corner_list = []    #2d points, ~imgpoints
@@ -38,25 +38,24 @@ def draw(img, imgpts):
     return img
 
 while(True):      
-    # Capture the video frame
-    # by frame
     ret, frame = vid.read()
+    frame = cv2.flip(frame,1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
   
     if ret:
         found, corners = cv2.findChessboardCorners(gray, board_dims, chessboard_flags)
         if found:
+            #part 1
             objpoints.append(objp)
             corners = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), corner_flags)
             corner_list.append(corners)
             cv2.drawChessboardCorners(gray, board_dims, corners, found)
 
+            #part 2
             ret, mtx, dist, _, _ = cv2.calibrateCamera(objpoints, corner_list, gray.shape[::-1], None, None)
-            retval, rvec, tvec = cv2.solvePnP(objp, corners, mtx, dist)
-
-            imgpts, jac = cv2.projectPoints(axisBoxes, rvec, tvec, mtx, dist)
-
-            frame = draw(frame, corners, imgpts)
+            _, rvec, tvec = cv2.solvePnP(objp, corners, mtx, dist)
+            imgpts, _ = cv2.projectPoints(axisBoxes, rvec, tvec, mtx, dist)
+            frame = draw(frame, imgpts)
             
         print(i, found)
         cv2.imshow('frame', frame)
